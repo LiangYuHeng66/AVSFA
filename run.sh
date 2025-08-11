f52c420f417bdf953ea9eafdb40e295b487f0dc9
liangyuheng <162766432+LiangYuHeng66@users.noreply.github.com>
2025-08-11 20:59:56 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DATASET_NAME="CUHK-PEDES"
+
+CUDA_VISIBLE_DEVICES=0 \
+python finetune.py \
+--name finetune_CUHK-PEDES \
+--img_aug \
+--batch_size 128 \
+--dataset_name $DATASET_NAME \
+--loss_names 'TAL+id+VTC+FGSM' \
+--num_epoch 30 \
+--root_dir ../../data \
+--ot_gamma 1.0 \
+--img_k_ratio 0.7 \
+--i_feats_fg_gamma 0.1 \
+--finetune The_pretrained_checkpoint
